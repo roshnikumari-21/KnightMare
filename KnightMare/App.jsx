5a1638a30bb06289931d2cbf4688f85346091f19
ayush kumar <ayush2422005@gmail.com>
2025-03-01 12:50:06 +0530

    path and bug fix

---
+++ KnightMare/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+import './global.css'
+
+
+
+function App() {
+
+
+  return (
+    <View>
+      <Text className="text-black text-lg font-bold">
+        hello
+      </Text>
+    </View>
+  );
+}
+
+export default App;
