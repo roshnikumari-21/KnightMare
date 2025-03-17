6407ed916e0d4d1590a77e325aea650ec1e3acbf
Sakshi <astrosakshisingh@gmail.com>
2025-03-17 08:24:41 +0530

    added chessboard componenet

---
+++ src/components/Play.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import ChessBoard from "./ChessBoard";
+
+const Play = () => {
+    const [showBoard, setShowBoard] = useState(false);
+
+    const loadChessBoard = () => {
+        setShowBoard(true);
+    }
+
+    return (
+        <div>
+            <button 
+                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                onClick={loadChessBoard}
+            >
+                Play
+            </button>
+            {showBoard && <ChessBoard />}
+        </div>
+    )
+}
+
+export default Play;
