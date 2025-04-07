a5f4ac810d6a200665f80a7a130bd3e6818bcb54
ayush kumar <ayush2422005@gmail.com>
2025-04-07 18:06:36 +0530

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),]
+  plugins: [react(), tailwindcss(),],
+  server: {
+    host: '0.0.0.0', // Allow access from any device
+    port: 5173, // Change if needed
+    strictPort: true, // Ensure the same port is used
+    allowedHosts: [
+      'campbell-ratios-madness-reviews.trycloudflare.com' // Add your Cloudflare Tunnel URL here
+    ],
+  }
 })
