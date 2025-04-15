215c1c7c274ca1edb49440a02806e82983d59fb3
aprajita <aprajitakumarithakur5555@gmail.com>
2025-04-15 13:11:29 +0530

    fix fix

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -101,7 +101,7 @@ const RulesPage = () => {
 
       {/* Glowing orbs */}
       <div className="absolute top-1/4 right-1/4 w-64 h-64 bg-blue-600 rounded-full opacity-10 blur-3xl"></div>
-      <div className="absolute bottom-1/4 left-1/4 w-96 h-96 bg-purple-600 rounded-full opacity-10 blur-3xl"></div>
+      <div className="absolute bottom-1/4 left-1/4 w-64 h-64 bg-purple-600 rounded-full opacity-10 blur-3xl"></div>
 
       {/* Loading state */}
       <AnimatePresence>
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -86,8 +86,8 @@ const Profile = () => {
 
       {/* Glowing orbs */}
       <div className="absolute top-1/4 right-1/4 w-64 h-64 bg-blue-600 rounded-full opacity-10 blur-3xl"></div>
-      <div className="absolute bottom-1/4 left-1/4 w-96 h-96 bg-purple-600 rounded-full opacity-10 blur-3xl"></div>
-      <div className="absolute bottom-1/2 left-1/2 w-96 h-96 bg-pink-600 rounded-full opacity-8 blur-3xl"></div>
+      <div className="absolute bottom-1/4 left-1/4 w-64 h-64 bg-purple-600 rounded-full opacity-10 blur-3xl"></div>
+      <div className="absolute bottom-1/2 left-1/2 w-32 h-32 bg-pink-600 rounded-full opacity-8 blur-3xl"></div>
       <div className="absolute bottom-1/4 left-1 w-96 h-96 bg-purple-700 rounded-full opacity-8 blur-3xl"></div>
 
       {/* Loading state */}
